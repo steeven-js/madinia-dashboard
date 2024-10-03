463c28a956f44c65844f1ff158d87d97c9e3d918
steeven-js <jacques.steeven@gmail.com>
2024-10-02 21:37:31 -0400

    Refactor router to conditionally skip home page based on CONFIG settings

--- src/routes/sections/index.jsx
+++ src/routes/sections/index.jsx
@@ -2,8 +2,8 @@ import { lazy, Suspense } from 'react';
 import { Navigate, useRoutes } from 'react-router-dom';
 
 import { MainLayout } from 'src/layouts/main';
-
 import { SplashScreen } from 'src/components/loading-screen';
+import { CONFIG } from 'src/config-global';
 
 import { authRoutes } from './auth';
 import { mainRoutes } from './main';
@@ -11,24 +11,20 @@ import { authDemoRoutes } from './auth-demo';
 import { dashboardRoutes } from './dashboard';
 import { componentsRoutes } from './components';
 
-// ----------------------------------------------------------------------
-
 const HomePage = lazy(() => import('src/pages/home'));
 
 export function Router() {
   return useRoutes([
     {
       path: '/',
-      /**
-       * Skip home page
-       * element: <Navigate to={CONFIG.auth.redirectPath} replace />,
-       */
-      element: (
+      element: CONFIG.auth.skip ? (
         <Suspense fallback={<SplashScreen />}>
           <MainLayout>
             <HomePage />
           </MainLayout>
         </Suspense>
+      ) : (
+        <Navigate to={CONFIG.auth.redirectPath} replace />
       ),
     },
 
